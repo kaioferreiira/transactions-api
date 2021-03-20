65c8d97d60737e225e5ec0bb5a4a8b34a853961c
Kaio Ferreira <kaio.costa@ntconsult.com.br>
2021-03-20 14:44:38 -0300

    Create exception handler para bad request

---
+++ src/main/java/br/com/transaction/handler/RestExceptionHandler.java
@@ -0,0 +1,29 @@
+package br.com.transaction.handler;
+
+import br.com.transaction.exception.BadRequestException;
+import br.com.transaction.exception.BadRequestExceptionDetails;
+import java.time.LocalDateTime;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@Log4j2
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<BadRequestExceptionDetails> handleBadRequestException(BadRequestException bre) {
+        return new ResponseEntity<>(
+                BadRequestExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request Exception, Check the Documentation")
+                        .details(bre.getMessage())
+                        .developerMessage(bre.getClass().getName())
+                        .build(), HttpStatus.BAD_REQUEST);
+    }
+
+}
